d58ed638eb4bb21ae35714b93f3c21d7cb089244
YSCJustin <yscjustin@gmail.com>
2024-09-05 19:23:01 +0800

    Updated to fit 24/25

--- index.js
+++ index.js
@@ -3,83 +3,29 @@ document.addEventListener("DOMContentLoaded", async () => {
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').innerHTML = currentYear;
 
-  const countdownTimestamp = 1693786800; 
-  const countdownTimestampu = 1698107400;
-  const countdownTimestamp2 = 1701736200;
-  const countdownTimestamp3 = 1710721800;
-  const countdownTimestamp4 = 1717374600;
+
   const countdownTimestamp5 = 1725236400;
+  const countdownTimestamp6 = 1730075400;
+  const countdownTimestamp7 = 1735864200;
+  const countdownTimestamp8 = 1742776200;
+  const countdownTimestamp9 = 1748997000;
+  const countdownTimestamp10 = 1756686000;
 
 
 
-  const countdownElement = document.getElementById('countdown');
-  const countdownElementu = document.getElementById('countdownu');
-  const countdownElement2 = document.getElementById('countdown2');
-  const countdownElement3 = document.getElementById('countdown3');
-  const countdownElement4 = document.getElementById('countdown4');
+
   const countdownElement5 = document.getElementById('countdown5');
+  const countdownElement6 = document.getElementById('countdown6');
+  const countdownElement7 = document.getElementById('countdown7');
+  const countdownElement8 = document.getElementById('countdown8');
+  const countdownElement9 = document.getElementById('countdown9');
+  const countdownElement10 = document.getElementById('countdown10');
  
-  updateTime();
+
   let datenow = 1000 - (Date.now() % 1000);
-  setTimeout(updateTime, datenow);
-
-  setInterval(() => {
-    
-    const currentTimestamp = Math.round(Date.now() / 1000);
-   
-
-    const timeDifference = countdownTimestamp - currentTimestamp;
-    const timeDifferenceu = countdownTimestampu - currentTimestamp;
-    const timeDifference2 = countdownTimestamp2 - currentTimestamp;
-    const timeDifference3 = countdownTimestamp3 - currentTimestamp;
-    const timeDifference4 = countdownTimestamp4 - currentTimestamp;
-    const timeDifference5 = countdownTimestamp5 - currentTimestamp;
-
- 
-    if (timeDifference <= 0) {
-     
-      countdownElement.innerHTML = `School started ${formatTime(Math.abs(timeDifference))} ago`;
-    } else {
-
-      countdownElement.innerHTML = `${formatTime(timeDifference)} left`;
-    }
-    if (timeDifferenceu <= 0) {
-                 
-      countdownElementu.innerHTML = `1st UT started ${formatTime(Math.abs(timeDifferenceu))} ago`;
-    }  else {
-        
-        countdownElementu.innerHTML = `${formatTime(timeDifferenceu)} left`;
-    }                          
-
-    if (timeDifference2 <= 0) {
-     
-      countdownElement2.innerHTML = `Mid Year Exam started ${formatTime(Math.abs(timeDifference2))} ago. Good Luck!`;
-    } else {
-
-      countdownElement2.innerHTML = `${formatTime(timeDifference2)} left`;
-    }
-    if (timeDifference3 <= 0) {
-     
-      countdownElement3.innerHTML = `2nd UT started ${formatTime(Math.abs(timeDifference3))} ago. Good Luck!`;
-    } else {
-
-      countdownElement3.innerHTML = `${formatTime(timeDifference3)} left`;
-    }
-    if (timeDifference4 <= 0) {
-     
-      countdownElement4.innerHTML = `Final Exam started ${formatTime(Math.abs(timeDifference4))} ago. Best of Luck!`;
-    } else {
-
-      countdownElement4.innerHTML = `${formatTime(timeDifference4)} left`;
-    }
-    if (timeDifference5 <= 0) {
-     
-      countdownElement5.innerHTML = `School 24/25 started ${formatTime(Math.abs(timeDifference5))} ago. Wow.`;
-    } else {
-
-      countdownElement5.innerHTML = `${formatTime(timeDifference5)} left`;
-    }
-  }, 1000);
+  setTimeout(async () => {
+    setInterval(updateTime, 1000);
+  }, datenow);
 
 
 
@@ -89,55 +35,59 @@ document.addEventListener("DOMContentLoaded", async () => {
     const currentTimestamp = Math.floor(Date.now() / 1000);
    
 
-    const timeDifference = countdownTimestamp - currentTimestamp;
-    const timeDifferenceu = countdownTimestampu - currentTimestamp;
-    const timeDifference2 = countdownTimestamp2 - currentTimestamp;
-    const timeDifference3 = countdownTimestamp3 - currentTimestamp;
-    const timeDifference4 = countdownTimestamp4 - currentTimestamp;
+
     const timeDifference5 = countdownTimestamp5 - currentTimestamp;
+    const timeDifference6 = countdownTimestamp6 - currentTimestamp;
+    const timeDifference7 = countdownTimestamp7 - currentTimestamp;
+    const timeDifference8 = countdownTimestamp8 - currentTimestamp;
+    const timeDifference9 = countdownTimestamp9 - currentTimestamp;
+    const timeDifference10 = countdownTimestamp10 - currentTimestamp;
+
  
-    if (timeDifference <= 0) {
-     
-      countdownElement.innerHTML = `School started ${formatTime(Math.abs(timeDifference))} ago`;
-    } else {
 
-      countdownElement.innerHTML = `${formatTime(timeDifference)} left`;
-    }
-    if (timeDifferenceu <= 0) {
-                 
-      countdownElementu.innerHTML = `1st UT started ${formatTime(Math.abs(timeDifferenceu))} ago`;
-    }  else {
-        
-        countdownElementu.innerHTML = `${formatTime(timeDifferenceu)} left`;
-    }      
-    if (timeDifference2 <= 0) {
-     
-      countdownElement2.innerHTML = `Mid Year Exam started ${formatTime(Math.abs(timeDifference2))} ago. Good Luck!`;
-    } else {
-
-      countdownElement2.innerHTML = `${formatTime(timeDifference2)} left`;
-    }
-    if (timeDifference3 <= 0) {
-     
-      countdownElement3.innerHTML = `2nd UT started ${formatTime(Math.abs(timeDifference3))} ago. Good Luck!`;
-    } else {
-
-      countdownElement3.innerHTML = `${formatTime(timeDifference3)} left`;
-    }
-    if (timeDifference4 <= 0) {
-     
-      countdownElement4.innerHTML = `Final Exam started ${formatTime(Math.abs(timeDifference4))} ago. Best of Luck!`;
-    } else {
-
-      countdownElement4.innerHTML = `${formatTime(timeDifference4)} left`;
-    }
     if (timeDifference5 <= 0) {
      
-      countdownElement5.innerHTML = `School 24/25 started ${formatTime(Math.abs(timeDifference5))} ago. Wow.`;
+      countdownElement5.innerHTML = `School 24/25 started ${formatTime(Math.abs(timeDifference5))} ago.`;
     } else {
 
       countdownElement5.innerHTML = `${formatTime(timeDifference5)} left`;
     }
+    if (timeDifference6 <= 0) {
+     
+      countdownElement6.innerHTML = `1st Uniform Test started ${formatTime(Math.abs(timeDifference6))} ago.`;
+    } else {
+
+      countdownElement6.innerHTML = `${formatTime(timeDifference6)} left`;
+    }
+    if (timeDifference7 <= 0) {
+     
+      countdownElement7.innerHTML = `Mid-Year Examination started ${formatTime(Math.abs(timeDifference7))} ago. Good luck.`;
+    } else {
+
+      countdownElement7.innerHTML = `${formatTime(timeDifference7)} left`;
+    }
+    if (timeDifference8 <= 0) {
+     
+      countdownElement8.innerHTML = `2nd Uniform Test started ${formatTime(Math.abs(timeDifference8))} ago.`;
+    } else {
+
+      countdownElement8.innerHTML = `${formatTime(timeDifference8)} left`;
+    }
+    if (timeDifference9 <= 0) {
+     
+      countdownElement9.innerHTML = `Final Examination started ${formatTime(Math.abs(timeDifference9))} ago. Good luck.`;
+    } else {
+
+      countdownElement9.innerHTML = `${formatTime(timeDifference9)} left`;
+    }
+    if (timeDifference10 <= 0) {
+     
+      countdownElement10.innerHTML = `School 25/26 started ${formatTime(Math.abs(timeDifference10))} ago.`;
+    } else {
+
+      countdownElement10.innerHTML = `${formatTime(timeDifference10)} left`;
+    }
+
   }
   function formatTime(time) {
     const days = Math.floor(time / 86400);
